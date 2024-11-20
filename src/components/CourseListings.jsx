c48dc966d69a788fa81879ffc1516c9ec1f8a025
korodhsoaqoon <korodhsoaqoon1@gmail.com>
2024-11-20 10:02:35 +0300

    Data Fetching using useEffect Hook

--- src/components/CourseListings.jsx
+++ src/components/CourseListings.jsx
@@ -1,7 +1,21 @@
 import CourseListing from "./CourseListing";
-import courses from "../courses.json";
+// import courses from "../courses.json";
+import { useState, useEffect } from "react";
 const CourseListings = ({ isHome }) => {
-  const courseListing = isHome ? courses.slice(0, 3) : courses;
+  // const courseListing = isHome ? courses.slice(0, 3) : courses;
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const apiUrl = isHome
+        ? "http://localhost:5000/courses?_limit=3"
+        : "http://localhost:5000/courses";
+      const res = await fetch(apiUrl);
+      const data = await res.json();
+      setCourses(data);
+    };
+    fetchCourses();
+  }, []);
   return (
     <>
       <section className="bg-blue-50 px-4 py-10">
@@ -11,7 +25,7 @@ const CourseListings = ({ isHome }) => {
           </h1>
           {/* <!-- Courses grid --> */}
           <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
-            {courseListing.map((course) => (
+            {courses.map((course) => (
               <CourseListing course={course} key={course.id} />
             ))}
           </div>
